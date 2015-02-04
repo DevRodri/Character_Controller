c5efac5d656274a8e0912766dd45e9248ef9dc46
DevRodri <rodry.fib@gmail.com>
2015-02-04 13:39:21 +0100

    Seguimiento Camara y Controlador físico
    mejorado script de seguimiento de cámara y hecho un controlador de
    personaje mediante fuerzas físicas.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
+    //variables
+    public GameObject target;
+    public float distance;
+    public float height;
+    public float smoothMove;
+
+    private Vector3 offset;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        this.transform.position = target.transform.position + (-target.transform.forward * distance) + (target.transform.up * height);
+        this.transform.rotation = target.transform.rotation;
+
+        offset = this.transform.position - target.transform.position;	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void LateUpdate () {
+
+        Quaternion destRotation = Quaternion.identity;
+        destRotation.eulerAngles = target.transform.rotation.eulerAngles;
+
+        offset.y = height;
+        offset.z = -distance;
+
+        Vector3 destPos = target.transform.position + (destRotation * offset);
+
+        this.transform.position = Vector3.Lerp(this.transform.position, destPos, smoothMove * Time.deltaTime);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, destRotation, smoothMove * Time.deltaTime);
+
+        this.transform.LookAt(target.transform.position);
+        
 	}
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public float turnSpeed;
+    public float jumpForce;
+
+    public float gravity;
+
+    private bool isGrounded = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (isGrounded)
+        {
+            /*REHACER EL MOVIMIENTO CON FUERZAS */
+            //Player Movement
+            this.transform.Translate(0, 0, Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime);
+            this.transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.fixedDeltaTime, 0);
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                //calcular la dirección de salto
+                Vector3 jumpDir = (this.transform.forward * Input.GetAxis("Vertical")) + this.transform.up;
+
+
+                this.rigidbody.AddForce(jumpDir * jumpForce, ForceMode.Impulse);
+                isGrounded = false;
+            }
+        }
+
+        //apply gravity
+        if(!isGrounded)
+        {
+            this.rigidbody.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+        }	
+	}
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        isGrounded = false;
+    }
+}
